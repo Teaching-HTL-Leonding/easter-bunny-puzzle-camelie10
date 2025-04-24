3872028ccf07422535e59d9d707566cff9feab6b
camelie10 <amelietingtingchen@gmail.com>
2025-04-24 10:27:13 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import p5 from 'p5';
 
 let bunnyImage: p5.Image;
-const tiles = 5;
+const TILES = 3;
+
+const randomTileX: number[][] = [];
+const randomTileY: number[][] = [];
 
 const p = new p5((sketch) => {
   sketch.setup = setup;
-  sketch.preload = preload;
   sketch.draw = draw;
+  sketch.preload = preload;
 });
 
 function preload() {
@@ -15,19 +18,49 @@ function preload() {
 
 function setup() {
   p.createCanvas(500, 500);
+  p.background('red');
+  const tileSize = 500 / TILES;
+  for (let x = 0; x < TILES; x++) {
+      randomTileX[x] = [];
+      randomTileY[x] = [];
+      for (let y = 0; y < TILES; y++) {
+          randomTileX[x][y] = Math.floor(p.random(TILES)) * tileSize;
+          randomTileY[x][y] = Math.floor(p.random(TILES)) * tileSize;
+        }
+    }
 }
 
 function draw() {
-  p.background('white');
-  let tileSize = bunnyImage.width/tiles;
-  
-  p.stroke('white');
-  p.noFill();
-  p.strokeWeight(2);
-  for (let zeile = 0; zeile <= tiles; zeile++) {
-    for (let i = 0; i <= tiles; i++) {
-      p.image(bunnyImage, tileSize * i, tileSize * zeile, tileSize, tileSize, tileSize * i, tileSize * zeile, tileSize, tileSize);
-      p.rect(tileSize * i, tileSize * zeile, tileSize, tileSize);
+    /*p.image(bunnyImage, 
+    25, 25,     // Left/upper corner
+    450, 450,   // Total image width/height
+    25, 25,     // Left/upper cut away
+    450, 450);  // Same as total image width/height
+    // (if image should not be distorted)
+    p.image(bunnyImage, 0,         0, 500 / 3, 500 / 3, 0,         0, 500 / 3, 500 / 3);#
+    p.image(bunnyImage, 500 / 3,   0, 500 / 3, 500 / 3, 500 / 3,   0, 500 / 3, 500 / 3);
+    p.image(bunnyImage, 500 / 3*2, 0, 500 / 3, 500 / 3, 500 / 3*2, 0, 500 / 3, 500 / 3);
+    
+    p.image(bunnyImage, 0,         500/3, 500 / 3, 500 / 3, 0,         500/3, 500 / 3, 500 / 3);
+    p.image(bunnyImage, 500 / 3,   500/3, 500 / 3, 500 / 3, 500 / 3,   500/3, 500 / 3, 500 / 3);
+    p.image(bunnyImage, 500 / 3*2, 500/3, 500 / 3, 500 / 3, 500 / 3*2, 500/3, 500 / 3, 500 / 3);*/
+    p.noFill();
+    p.stroke('white');
+    p.strokeWeight(2);
+    for (let x = 0; x < TILES; x++) {
+        for (let y = 0; y < TILES; y++) {
+            const imageX = randomTileX[x][y];
+            const imageY = randomTileY[x][y];
+            const tileSize = 500 / TILES;
+
+            if(x !=TILES-1 || y!=TILES-1){
+      p.rect(imageX, imageY, tileSize, tileSize);
+      p.image(bunnyImage, 
+        tileSize*x, tileSize*y, 
+        tileSize, tileSize, 
+        imageX, imageY, 
+        tileSize, tileSize);
+      }
     }
   }
 }
